6aaef996e4ab4d1530fda806ebc8bdff467d82cf
eatrice <i@eatrice.cn>
2023-06-17 11:20:07 +0800

    fix the returned result

--- testSQL.sql
+++
@@ -1,7 +0,0 @@
--- database: /Users/eatrice/Library/CloudStorage/OneDrive-Personal/python/PlateTheroy/algserver/db.sqlite3
-
--- 要运行整个文件，请按窗口右上角的▷按钮。
-
-SELECT * FROM "foundationpit_foundationcalculationtask";
-
-DELETE FROM foundationpit_foundationcalculationtask WHERE status=2;
