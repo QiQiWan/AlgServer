a9c62da55fb6744f7a70dac1f4b0c82c0f1ea0ca
eatrice <wanqiweiyy@outlook.com>
2025-03-16 20:58:15 +0800

    Add single mesh

--- algserver/settings.py
+++ algserver/settings.py
@@ -21,11 +21,11 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-# SECRET_KEY = os.environ['SECRET_KEY']
-# SECRET_ID = os.environ['SECRET_ID']
+SECRET_KEY = os.environ['SECRET_KEY']
+SECRET_ID = os.environ['SECRET_ID']
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['0.0.0.0', '127.0.0.1', 'alg.eatrice.cn']
 
